e5c216fcfb5297e23112b0b0dab28060e352be5e
Tony <tony.wang.4489@student.uu.se>
2018-01-19 11:03:49 +0100

    Updated audio paths again to fix export error

--- src/audio/audio_paths.js
+++ src/audio/audio_paths.js
@@ -1,4 +1,5 @@
 /**
  * Created by Tony Wang on 1/19/2018.
  */
-export const audio_path = 'https://vignette.wikia.nocookie.net/leagueoflegends/images/3/34/MasterYi.taunt01.ogg/revision/latest?cb=20130713221434'
+export const audio_path = 'https://vignette.wikia.nocookie.net/leagueoflegends/images/3/34/MasterYi.taunt01.ogg/revision/latest?cb=20130713221434';
+
